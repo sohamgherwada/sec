b6b32cc02ef1b0ed9c1556ecabb889a05de19281
sohamgherwada <112994330+sohamgherwada@users.noreply.github.com>
2024-05-24 02:06:29 -0400

    Add files via upload

---
+++ testpy/web.py
@@ -0,0 +1,27 @@
+from flask import Flask, Response
+import cv2
+
+app = Flask(__name__)
+cap = cv2.VideoCapture(0)
+
+def generate_frames():
+    while True:
+        success, frame = cap.read()
+        if not success:
+            break
+        else:
+            ret, buffer = cv2.imencode('.jpg', frame)
+            frame = buffer.tobytes()
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+@app.route('/')
+def index():
+    return "<img src='/video_feed'>"
+
+if __name__ == '__main__':
+    app.run(debug=True)
